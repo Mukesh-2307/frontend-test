0693e4d8b5001e04a4c56520849ac8f8478caed3
MukeshDalai <mukeshdalai.2307@gmail.com>
2025-11-20 00:21:39 +0530

    update: adding navbar component

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div className='flex justify-between items-center font-bold text-white'>
+            <div>
+                logo
+            </div>
+
+            <div>
+                nav-links
+            </div>
+            <div>
+                icons
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
